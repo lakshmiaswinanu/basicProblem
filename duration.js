5799292358894401c08b5d75dc41334af7ad25ff
lakshmiaswinanu <lakshmiaswin@gmail.com>
2023-04-20 12:20:50 +0530

    Renamed marksheet file.

--- duration.js
+++ duration.js
@@ -1,37 +1,36 @@
 
 const monday = [
-  {
-      'name'     : 'Write a tutorial',
-      'duration' : 180
-  },
-  {
-      'name'     : 'Some web development',
-      'duration' : 120
-  }
+    {
+        'name': 'Write a tutorial',
+        'duration': 180
+    },
+    {
+        'name': 'Some web development',
+        'duration': 120
+    }
 ];
 
 const tuesday = [
-  {
-      'name'     : 'Keep writing that tutorial',
-      'duration' : 240
-  },
-  {
-      'name'     : 'Some more web development',
-      'duration' : 180
-  },
-  {
-      'name'     : 'A whole lot of no',
-      'duration'  :120
-  },
-{
-      'name'     : 'A whole lot of nothing',
-      'duration'  : 240
-  }
+    {
+        'name': 'Keep writing that tutorial',
+        'duration': 240
+    },
+    {
+        'name': 'Some more web development',
+        'duration': 180
+    },
+    {
+        'name': 'A whole lot of no',
+        'duration': 120
+    },
+    {
+        'name': 'A whole lot of nothing',
+        'duration': 240
+    }
 ];
 
-const duration =monday.map((x)=> x.duration/60);
+const duration = monday.map((x) => x.duration / 60);
 const greatDuration = ((x) => Math.min(x.duration) >= 2);
 console.log(greatDuration);
-const getDuration =tuesday.map((x)=> x.duration/60);
+const getDuration = tuesday.map((x) => x.duration / 60);
 console.log(getDuration);
-
