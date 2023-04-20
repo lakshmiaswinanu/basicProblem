e5a463293fa2f9c67f9af35af43660e18d428b5e
lakshmiaswinanu <lakshmiaswin@gmail.com>
2023-04-20 12:19:47 +0530

    Changed bankAccounts file.

--- bankAccounts.js
+++ bankAccounts.js
@@ -19,7 +19,7 @@ const balances = {
   '2': 2000,
   '3': 0,
 };
- 
+
 const transactions = [
   {
     accountNo: 1,
@@ -47,41 +47,43 @@ const transactions = [
     amount: 200,
   },
   {
-    accountNo:3,
+    accountNo: 3,
     type: 'deposit',
     amount: 200,
   },
 ];
 
-var updateBalancesWithTransactions = function() {	// Implement transaction code here.}; var displayBalances = function() {	// Implement display code here.}; // Do not change below this line.var main = function() {	console.log('Balances before transactions');	displayBalances();	updateBalancesWithTransactions();	console.log('Balances after transactions');	displayBalances();} main();
+var updateBalancesWithTransactions = function () {	// Implement transaction code here.}; var displayBalances = function() {	// Implement display code here.}; // Do not change below this line.var main = function() {	console.log('Balances before transactions');	displayBalances();	updateBalancesWithTransactions();	console.log('Balances after transactions');	displayBalances();} main();
 }
 
-var updateBalancesWithTransactions = function() {
-// Implement transaction code here.
+var updateBalancesWithTransactions = function () {
+  // Implement transaction code here.
 };
-const getName = accounts.map((x)=>x.name);
-const getAccountNo =(x)=>x.accountNo;
-const getBalance = (x)=>balances[x];
+
+const getName = accounts.map((x) => x.name);
+const getAccountNo = (x) => x.accountNo;
+const getBalance = (x) => balances[x];
 
 
 
-const displayBalance =(accounts)=> accounts.map((acc)=>({...acc, balances : getBalance([acc.accountNo])})); 
-   
-  
-const updateBalances=(transactions)=> transactions.map((acc) => {
-const transaction = acc.type === 'withdrawal' 
-? balances[acc.accountNo] = getBalance([acc.accountNo]) - acc.amount 
-    : balances[acc.accountNo]= getBalance([acc.accountNo]) + acc.amount;
-    return transaction;
-     }  )
+const displayBalance = (accounts) => accounts.map((acc) => ({ ...acc, balances: getBalance([acc.accountNo]) }));
+
+
+const updateBalances = (transactions) => transactions.map((acc) => {
+  const transaction = acc.type === 'withdrawal'
+    ? balances[acc.accountNo] = getBalance([acc.accountNo]) - acc.amount
+    : balances[acc.accountNo] = getBalance([acc.accountNo]) + acc.amount;
+  return transaction;
+})
+
 // Do not change below this line.
-var main = function() {
-  
-console.log('Balances before transactions');
-console.log(displayBalance(accounts));
-console.log('Balances after transactions');
-updateBalances(transactions);
- console.log(displayBalance(accounts));
+var main = function () {
+
+  console.log('Balances before transactions');
+  console.log(displayBalance(accounts));
+  console.log('Balances after transactions');
+  updateBalances(transactions);
+  console.log(displayBalance(accounts));
 }
 
 main();
